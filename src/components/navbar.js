dc92fd02d3227e82af249988d8950ba10c4014e4
Lalit Kishor <lkbksahawa@gmail.com>
2024-12-05 10:47:08 +0530

    Made navbar responsive and seminavbar fixed below the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,27 @@
 import { NavLink, Outlet } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export const Navbar = () => {
 
     const activeState = (isActive) => ({
 
-            color: isActive ? '#FD4215' : '',
-            fontWeight: isActive ? 'bold' : ''
-    
+        color: isActive ? '#FD4215' : '',
+        fontWeight: isActive ? 'bold' : ''
+
     });
 
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top mb-5">
                 <div className="container-fluid">
-                    {/* <NavLink className="navbar-brand" to="#">FitBit</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button> */}
-
+                    
                     <span className="material-symbols-outlined" style={{color: "yellow", marginLeft: "5px", marginRight: "21px"}}>
                     auto_transmission
                     </span>
-
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
@@ -32,10 +31,6 @@ export const Navbar = () => {
                                 <NavLink className="nav-link" to="/about" style={({ isActive }) => activeState(isActive)} >Profile</NavLink>
                             </li>
                         </ul>
-                        {/* <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form> */}
                         
                         <NavLink to="/logout">
                             <button className="btn btn-outline-danger" type="button">Logout</button>
--- src/components/semiNavbar.js
+++ src/components/semiNavbar.js
@@ -1,25 +1,76 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export const ProjectNavbar = () => {
     return (
-        <nav class="navbar navbar-light bg-body-tertiary">
-            <div class="container-fluid">
-                <form class="d-flex input-group w-auto">
-                    <input
-                        type="search"
-                        class="form-control rounded"
-                        placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="search-addon"
-                    />
-                    <span class="input-group-text border-0" id="search-addon">
-                        <i class="fas fa-search"></i>
-                    </span>
+        <nav className="navbar navbar-light rounded" style={{ backgroundColor: '#384052', marginBottom: '5px' }}>
+            <div className="container-fluid py-1">
+                <form className="d-flex justify-content-between align-items-center w-100">
+                
+                    <div className="input-group w-50">
+                        <input
+                            type="search"
+                            className="form-control"
+                            placeholder="Search"
+                            aria-label="Search"
+                            aria-describedby="search-addon"
+                        />
+                        <button type="submit" className="input-group-text border-0 btn btn-primary" id="search-addon">
+                            <i className="bi bi-search"></i>
+                        </button>
+                    </div>
+
+                    
+                    <div className="dropdown mx-2">
+                        <button
+                            className="btn btn-success dropdown-toggle"
+                            type="button"
+                            id="dropdownMenuCategory"
+                            data-bs-toggle="dropdown" // Fixed Bootstrap 5 toggle attribute
+                            aria-expanded="false"
+                        >
+                            Select Category
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuCategory">
+                            <li><a className="dropdown-item" href="#a">App Development</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#c">Artificial Intelligence</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#b">Blockchain</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#c">Cyber Security</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#c">Data Science</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#c">IOT</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#c">Web Development</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#c">Other</a></li>
+                        </ul>
+                    </div>
+
+                    
+                    <div className="dropdown">
+                        <button
+                            className="btn btn-success dropdown-toggle"
+                            type="button"
+                            id="dropdownMenuSort"
+                            data-bs-toggle="dropdown" // Fixed Bootstrap 5 toggle attribute
+                            aria-expanded="false"
+                        >
+                            Sort By
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuSort">
+                            <li><a className="dropdown-item" href="#a">Likes</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a className="dropdown-item" href="#b">Bookmarks</a></li>
+                        </ul>
+                    </div>
                 </form>
             </div>
         </nav>
     );
 };
-
